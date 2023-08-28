6076d23e547a27c9f050fcba0857152005d04e52
pulungw <pulungw@gmail.com>
2023-08-28 01:11:39 +0000

    Edit valid path in python code

--- python/simple_cars_test.py
+++ python/simple_cars_test.py
@@ -3,7 +3,7 @@ import pandas
 df = SAS.sd2df('sashelp.cars')
 
 ## Location to create the new sas7bdat file. Please modify to a path in your environment
-outpath = '/specify/folder/path/Data'       
+outpath = '/mnt/viya-share/data/tmp'       
 SAS.submit(f"libname mydata '{outpath}' ;")
 
 SAS.df2sd(df,'mydata.cars')
